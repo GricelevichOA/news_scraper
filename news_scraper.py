9e3946754e127d07391d90990ad1e1f1fc8a52bf
Oleg <gricelevich.oleg@gmail.com>
2020-02-06 23:06:18 +0300

    save news to dictionary

--- news_scraper.py
+++ news_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from abc import ABC, abstractmethod
+import json
 
 class SiteScraper(ABC):
 
@@ -8,16 +9,17 @@ class SiteScraper(ABC):
 	def get_news():
 		pass
 
-
 class TutByScraper(SiteScraper):
 	def __init__(self, url):
 		self.url = 'https://www.tut.by/'
+		self.name = 'tut.by'
 
 	def get_news(self):
 		res = requests.get(self.url)
 		soup = BeautifulSoup(res.content, 'html.parser')
 		all_news = soup.find('div', id='latest')
 		news_links = all_news.find_all('a', class_='entry__link io-block-link')
+		news_dict = []
 
 		for news in news_links:
 			article_header = news.find('span', class_='entry-head _title').text
@@ -26,19 +28,16 @@ class TutByScraper(SiteScraper):
 			article_soup = BeautifulSoup(article_res.content, 'html.parser')
 			article_body = article_soup.find('div', id='article_body').find_all('p')
 			article_text = ' '.join([paragraph.get_text() for paragraph in article_body])
-			
-			print(article_link)
-			print(article_header)			
-			print(article_text)
-			print(' ')
 
-			
+			news_dict.append({
+				"name": self.name,
+				"link": article_link,
+				"header": article_header,
+				"text": article_text
+			})
+		return news_dict
 
-
-		#print(news_links)
-
-bruh = TutByScraper('')
-
-bruh.get_news()		
+show_news = TutByScraper('')
+print(show_news.get_news())		
 
 
